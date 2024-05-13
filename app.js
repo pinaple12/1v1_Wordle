592e51594d3ae7c027739b102212628eb80feaab
Kai Tinder <ktinder2003@gmail.com>
2024-05-13 13:53:19 -0700

    azure account setup

--- app.js
+++ app.js
@@ -2,7 +2,32 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import sessions from 'express-session';
 
+import WebAppAuthProvider from 'msal-node-wrapper'
+
+const authConfig = {
+	auth: {
+		clientId: "",
+        authority: "",
+        clientSecret: "",
+        redirectUri: ""
+	},
+
+	system: {
+    	loggerOptions: {
+        	loggerCallback(loglevel, message, containsPii) {
+            	console.log(message);
+        	},
+        	piiLoggingEnabled: false,
+        	logLevel: 3,
+    	}
+	}
+};
+
+
+
+import models from './models.js';
 import usersRouter from './routes/users.js';
 
 import { fileURLToPath } from 'url';
@@ -19,6 +44,37 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const oneDay = 1000 * 60 * 60 * 24
+app.use(sessions({
+    secret: "this is some secret key I am making up p8kn fwlihftrn3oinyswnwd3in4oin",
+    saveUninitialized: true,
+    cookie: {maxAge: oneDay},
+    resave: false
+}))
+
+const authProvider = await WebAppAuthProvider.WebAppAuthProvider.initialize(authConfig);
+app.use(authProvider.authenticate());
+
+app.use((req, res, next) => {
+    req.models = models
+    next()
+});
+
 app.use('/users', usersRouter);
+
+app.get('/signin', (req, res, next) => {
+    return req.authContext.login({
+        postLoginRedirectUri: "/", // redirect here after login
+    })(req, res, next);
+
+});
+app.get('/signout', (req, res, next) => {
+    return req.authContext.logout({
+        postLogoutRedirectUri: "/", // redirect here after logout
+    })(req, res, next);
+
+});
+
+app.use(authProvider.interactionErrorHandler());
 
 export default app;
